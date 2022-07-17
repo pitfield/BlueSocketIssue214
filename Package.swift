ba1c503ffb1caf201d4dff660abc78ca408ce630
David Pitfield <david@codewins.com>
2022-07-17 08:18:57 -0700

    Add dependencies to Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BlueSocketIssue214",
+    dependencies: [
+        .package(url: "https://github.com/IBM-Swift/BlueSocket", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.3"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "BlueSocketIssue214",
+            dependencies: [
+                .product(name: "Socket", package: "BlueSocket"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]),
+        .testTarget(
+            name: "BlueSocketIssue214Tests",
+            dependencies: ["BlueSocketIssue214"]),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# BlueSocketIssue214
+
+A description of this package.
