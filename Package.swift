a1a960c3d35bc19cfb7688c66afcafd6e52f70eb
David Pitfield <david@codewins.com>
2022-07-18 16:27:42 -0700

    Get BlueSocket from fork

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "BlueSocketIssue214",
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/BlueSocket", from: "2.0.0"),
+        .package(url: "https://github.com/pitfield/BlueSocket", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.3"),
     ],
     targets: [
